76130405bd32003cdda31c9797638a4efc83336e
Karol <kakol.karol00@gmail.com>
2021-02-05 02:05:01 +0100

    add anon login

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,10 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
+
+
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // sign in anon
-
 
 
   Future signInAnon() async {
