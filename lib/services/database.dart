45b96bcbb95f83544c331d63f49952f73c8ae514
Karol <kakol.karol00@gmail.com>
2021-02-06 21:27:50 +0100

    create database service and add reference to a collection

---
+++ lib/services/database.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+
+  //collection reference
+
+  final CollectionReference brewCollection = FirebaseFirestore.instance.collection('brews');
+
+
+
+}
